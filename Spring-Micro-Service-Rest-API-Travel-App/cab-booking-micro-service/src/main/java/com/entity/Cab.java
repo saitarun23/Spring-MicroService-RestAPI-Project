876ca15ae2573113c31d9b67c9dda1f6cd0eaf38
saitarun23 <122889220+saitarun23@users.noreply.github.com>
2024-10-07 17:50:07 +0530

    done

--- Spring-Micro-Service-Rest-API-Travel-App/cab-booking-micro-service/src/main/java/com/entity/Cab.java
+++ Spring-Micro-Service-Rest-API-Travel-App/cab-booking-micro-service/src/main/java/com/entity/Cab.java
@@ -1,6 +1,5 @@
 package com.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,7 +12,6 @@ public class Cab {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true)
 	private int cid;
 	private String typeofcab;
 	private int seatcapacity;
